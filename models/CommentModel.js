b8209725aeebc44cc78ded27485b52cb26fb050e
Daniel Peretz <danielpertz22@gmail.com>
2024-11-19 16:33:24 +0200

    nodelspost+ nodelscoments

--- models/CommentModel.js
+++ models/CommentModel.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
-  text: String,
-  author: String,
-}, { timestamps: true });
+  content: {
+    type: String,
+    required: true,
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true,
+  },
+  owner: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true }); 
+
+const Comment = mongoose.model('Comment', commentSchema);
 
 module.exports = mongoose.model('Comment', commentSchema);
--- models/PostModel.js
+++ models/PostModel.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-  senderId: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: String,
+    required: false,
+  },
+  senderId: { 
+    type: String,
+    required: true, 
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Post', postSchema);
