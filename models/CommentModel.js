758695430d3ff3fa4853719295a17f3c846225b9
Daniel Peretz <danielpertz22@gmail.com>
2024-11-19 16:36:34 +0200

    test

--- models/CommentModel.js
+++ models/CommentModel.js
@@ -19,3 +19,7 @@ const commentSchema = new mongoose.Schema({
 const Comment = mongoose.model('Comment', commentSchema);
 
 module.exports = mongoose.model('Comment', commentSchema);
+
+
+
+const yphai = 8;
