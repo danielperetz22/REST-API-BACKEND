b8eea2f26edf6940f895a1dc11a291c67bd589a8
Hila Amsterdam <128729989+HillaAmsterdam@users.noreply.github.com>
2024-11-19 16:13:00 +0200

    mongo-connection

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-
 const dotenv = require("dotenv").config();
+const mongoose = require('mongoose');
 
 const port = process.env.PORT;
 
+app.use(express.json());
 
-
+mongoose.connect('mongodb://localhost:27017/mydb',)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
