5de8a927a4de34fdfddb0dd4939bfa5d8eadd93e
Hila Amsterdam <128729989+HillaAmsterdam@users.noreply.github.com>
2025-01-04 18:47:44 +0200

    .

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./server"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const port = process.env.PORT;
-server_1.default.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+(0, server_1.default)().then((app) => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 });
 //# sourceMappingURL=app.js.map
