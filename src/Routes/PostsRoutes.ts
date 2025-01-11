04b6285300754aad0fc21459833d6ccee36bafbe
Hila Amsterdam <128729989+HillaAmsterdam@users.noreply.github.com>
2025-01-11 10:53:12 +0200

    fix

--- src/Routes/PostsRoutes.ts
+++ src/Routes/PostsRoutes.ts
@@ -7,8 +7,5 @@ router.get('/filter/bySender', PostControllers.getPostsBySender);
 router.get('/all', PostControllers.getAllPosts);
 router.get('/:id', PostControllers.getPostById);
 router.put('/:id', PostControllers.updatePost);
-router.post('/', (req, res, next) => {
-    console.log('Request reached POST /');
-});
 router.delete('/delete/:id', PostControllers.deletePost);
 export default router;
