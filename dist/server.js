314c530a0fdb8ddf1a21c4b3dbd100cc1f524f79
Hila Amsterdam <128729989+HillaAmsterdam@users.noreply.github.com>
2025-01-12 13:12:22 +0200

    app&server

--- dist/server.js
+++ dist/server.js
@@ -9,29 +9,45 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const PostsRoutes_1 = __importDefault(require("./Routes/PostsRoutes"));
 const CommentRoutes_1 = __importDefault(require("./Routes/CommentRoutes"));
 const AuthRoutes_1 = __importDefault(require("./Routes/AuthRoutes"));
+const body_parser_1 = __importDefault(require("body-parser"));
 dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use('/post', PostsRoutes_1.default);
+app.use('/comment', CommentRoutes_1.default);
+app.use('/auth', AuthRoutes_1.default);
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Hila and Daniel's REST API",
+            version: "1.0.0",
+            description: "REST server including authentication using JWT",
+        },
+        servers: [{ url: "http://localhost:3000" }],
+    },
+    apis: ["./src/Routes/*.ts"],
+};
 const initApp = async () => {
-    const app = (0, express_1.default)();
-    app.use(express_1.default.json());
-    app.use((req, res, next) => {
-        console.log(`${req.method} ${req.url}`);
-        next();
+    return new Promise((resolve, reject) => {
+        const db = mongoose_1.default.connection;
+        db.on("error", (error) => {
+            console.error("Database connection error:", error);
+        });
+        db.once("open", () => {
+            console.log("Connected to database");
+        });
+        if (!process.env.MONGO_URI) {
+            console.error("initApplication UNDEFINED MONGO_URI");
+            reject();
+            return;
+        }
+        else {
+            mongoose_1.default.connect(process.env.MONGO_URI).then(() => {
+                resolve(app);
+            });
+        }
     });
-    app.use('/post', PostsRoutes_1.default);
-    app.use('/comment', CommentRoutes_1.default);
-    app.use('/auth', AuthRoutes_1.default);
-    const mongoUri = process.env.MONGO_URI;
-    if (!mongoUri) {
-        throw new Error("MONGO_URI is not defined in the environment variables");
-    }
-    try {
-        await mongoose_1.default.connect(mongoUri);
-        console.log("Connected to MongoDB");
-    }
-    catch (err) {
-        console.error("MongoDB connection error:", err);
-        throw err;
-    }
-    return app;
 };
 exports.default = initApp;
